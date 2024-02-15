4232a82bbd2f86ed014b709c6c5c55e2aa571979
Kitupala <kimmo.tuupanen@gmail.com>
2024-02-15 12:10:19 +0200

    Quick fix

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, createContext, useEffect, useState } from "react";
+import { ReactElement, createContext, useState } from "react";
 
 export type ProductType = {
   sku: string;
@@ -34,7 +34,7 @@ const ProductsContext = createContext<UseProductsContextType>(initContextState);
 type ChildrenType = { children?: ReactElement | ReactElement[] };
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
-  const [products, setProducts] = useState<ProductType[]>(initState);
+  const [products] = useState<ProductType[]>(initState);
 
   // DATA QUERY
   // useEffect(() => {
